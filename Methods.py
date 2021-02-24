61dec6d1d3921bebdb367c3e86799241d30f1834
Redut Anatoly <56587883+Apirol@users.noreply.github.com>
2021-02-24 12:28:57 +0700

    Added fibonacci method

--- Methods.py
+++ Methods.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, log
 from Init import InitReport
 
 SQRT5 = sqrt(5)
@@ -72,12 +72,45 @@ def golden_ratio(a, b, eps, filename):
     return numberOfCalc, a
 
 
-def Fibonacci(a, b, eps, filename):
+def calc_fibonacci(n: int):
+    return pow((1 + SQRT5) / 2, n) / SQRT5
+
+
+def fibonacci(a, b, eps, filename):
     numberOfCalc = 0
     current_lenght = b - a
     report = InitReport(filename)
     handle = report.get_handle()
 
-    return numberOfCalc, (a + b) / 2
+    n = round(log(SQRT5 * (b - a) / eps) / log((1 + SQRT5) / 2))
+    x1 = a + calc_fibonacci(n) / calc_fibonacci(n + 2) * (b - a)
+    x2 = a + calc_fibonacci(n + 1) / calc_fibonacci(n + 2) * (b - a)
+    f1 = function(x1)
+    f2 = function(x2)
+
+    while (abs(a - b) > eps):
+        numberOfCalc += 1
+
+        if (f1 < f2):
+            b = x2
+            x2 = x1
+            x1 = a + calc_fibonacci(n - numberOfCalc - 1) / calc_fibonacci(n - numberOfCalc + 1) * (b - a)
+            f2 = f1
+            f1 = function(x1)
+        else:
+            a = x1
+            x1 = x2
+            x2 = a + calc_fibonacci(n - numberOfCalc) / calc_fibonacci(n - numberOfCalc + 1) * (b - a)
+            f1 = f2
+            f2 = function(x2)
+
+        handle.write("{:f}\t{:f}\t""{:f}\t{:f}\t\t""{:f}\t\t\t{:f}\t\t""{:f}\t\t\t\t{:f}\n"
+                     .format(x1, x2, f1, f2, a, b, current_lenght, current_lenght / (b - a)))
+
+        current_lenght = b - a
+
+    return numberOfCalc, a
+
+
 
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from Methods import bisection_method, golden_ratio
+from Methods import bisection_method, golden_ratio, fibonacci
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    calc_number, result = bisection_method(-2, 20, 0.01, "bisection.txt")
+    calc_number, result = fibonacci(-2, 20, 0.01, "fibonacci.txt")
     print(calc_number, result)
 
